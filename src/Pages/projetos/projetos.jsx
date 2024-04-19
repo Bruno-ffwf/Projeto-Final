d5392e6ddf3ce7885c216ec3e855e772f5a2962e
Bruno <brunosoliveira659@gmail.comgit config --global user.email brunosoliveira659@gmail.comgit config --global user.email brunosoliveira659@gmail.com>
2024-04-19 12:10:27 -0300

    first commit

---
+++ src/Pages/projetos/projetos.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import Wiki from "./projeto assts/wiki.png"
+import Selfcare from "./projeto assts/selfcare.png"
+import Mc from "./projeto assts/mc.png"
+import Carousel from 'react-elastic-carousel'
+
+export default function projetos() {
+  return (
+<main className='mainprojetos'>
+<h1 className='h1projetos'>Projetos</h1>
+
+<Carousel>
+<div className='divprojetos'>
+  <a className='linkprojeotos' href="https://7hkjr2.csb.app/"><img className='imgprojetos' src={Wiki} alt="Wiki" /></a>
+  <figure className='figprojetos'>Mini Wiki feita em HTML</figure>
+</div>
+
+<div className='divprojetos'>
+  <a className='linkprojeotos' href="https://bruno-ffwf.github.io/Desafio-Final/"><img className='imgselfcare' src={Selfcare} alt="Selfcare" /></a>
+  <figure className='figprojetos'>Site Selfcare feito em HTML e CSS.</figure>
+</div>
+
+<div className='divprojetos'>
+  <a className='linkprojeotos' href="https://github.com/Bruno-ffwf/Desafio"><img className='imgprojetos' src={Mc} alt="Site McDonald's" /></a>
+  <figure className='figprojetos'>Site McDonald's feito em React.</figure>
+</div>
+</Carousel>
+
+</main>
+  )
+}
---
+++ src/componets/Header.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Placeholder from "./assets/placeholder.jpg"
+import Linkedin from "./assets/linkedin.png"
+import Github from "./assets/git.png"
+import Facebook from "./assets/facebook.png"
+
+
+
+export default function Header() {
+  return ( 
+<header>
+  <div className='divheader'>
+    <img className='icone' src={Placeholder} alt="" />
+    <h1 className='h1header'>Bruno Silva de Oliveira</h1>
+    <h2 className='h2header'>Front-end Developer</h2>
+  </div>
+
+<div className='divlinks'>
+  <a className='linkheader' href=""><img className='imgheader' src={Linkedin} alt="Linkedin" /></a>
+  <a className='linkheader' href="https://github.com/Bruno-ffwf"><img className='imgheader' src={Github} alt="Github" /></a>
+  <a className='linkheader' href=""><img className='imgheader' src={Facebook} alt="Facebook" /></a>
+</div>
+
+
+</header>
+  )
+}
