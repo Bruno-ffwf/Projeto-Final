e665fae9a7bf632f6034e40747e48a29aac53b30
Bruno-ffwf <147623144+Bruno-ffwf@users.noreply.github.com>
2024-04-29 09:35:27 -0300

    Add files via upload

--- src/componets/styled.jsx
+++ src/componets/styled.jsx
@@ -1,12 +1,10 @@
-import styled, { createGlobalStyle } from 'styled-components' 
-
+import styled, { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
 *{
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-    //background-color:#839697;
     background-color: #4169E1;
 }
 
@@ -15,9 +13,15 @@ export const GlobalStyle = createGlobalStyle`
     flex-direction: row;
     justify-content: space-evenly;
     padding-top: 20px;
-    background-color: #4169E1;
 }
 
+@media (max-width: 480px) {
+    .nav{
+        flex-direction: column;
+        align-items: center;
+        padding-top: 10px;
+    }
+}
 
 .link{
     font-size: 35px;
@@ -43,6 +47,13 @@ header{
     background: #4A5051; 
 }
 
+@media (max-width: 480px) {
+    header{
+        flex-direction: column;
+        align-items: center;
+    }
+}
+
 .divheader{
     padding-top: 30px;
     padding-left: 30px;
@@ -54,8 +65,8 @@ header{
     height: 200px;
     border: solid 5px darkgray;
     border-radius: 10px;
-    align-items: center;
 }
+
 
 .h1header{
     color: white;
@@ -73,6 +84,13 @@ header{
     background: #4A5051;
 }
 
+@media (max-width: 480px) {
+    .divlinks{
+        padding-top: 80px;
+        padding-right: 15px;
+    }
+}
+
 
 .linkheader{
     text-decoration: none;
@@ -84,10 +102,26 @@ header{
     height: 90px;
     padding-right: 30px;
     background: #4A5051;
+    position: relative;
+    animation: pulsar 1s ease-in-out infinite;
+}
+
+@media (max-width: 480px) {
+    .imgheader{
+        width: 80px;
+        height: 60px;
+        padding-right: 10px;
+        margin-left: 5px;
+        margin-right: 5px;
+        margin-bottom: 10px;
+        position: relative;
+        animation: pulsar 1s ease-in-out infinite;
+        top: 50%;
+        transform: translateX(19%);
+    }
 }
 
 .iniciomain{
-    //ackground-color: #839697;
     align-items: center;
     text-align: center;
     padding: 50px;
@@ -107,20 +141,46 @@ header{
     overflow: hidden;
     white-space: nowrap;
     animation: typing 2s steps(30) 1 forwards;
-
 }
 
 .h1inicio::after{
     animation: blink 1s infinite;
 }
 
+@media (max-width: 708px) {
+    .h1inicio{
+        font-size: 13px;
+        width: 100%;
+    }
+}
+
 .iconeinicio{
     border: solid 3px black;
-    border-radius: 400px;
+    border-radius: 50%;
     width: 300px;
     height: 300px;
 }
 
+@keyframes pulsar {
+    0% {
+        transform: scale(1);
+    }
+    50% {
+        transform: scale(1.1); /* Aumenta o tamanho em 10% */
+    }
+    100% {
+        transform: scale(1);
+    }
+}
+
+@media (max-width: 480px) {
+    .iconeinicio{
+        width: 100px;
+        height: 100px;
+        border-radius: 50%;
+        border-width: 1px;
+    }
+}
 
 .mainprojetos{
     text-align: center;
@@ -152,12 +212,30 @@ header{
     border: solid 3px black;
 }
 
+@media (max-width: 480px) {
+    .divprojetos{
+        width: 150px;
+        height: 180px;
+        border-radius: 12px;
+        border-width: 1pxlevado ;
+    }
+}
+
 .imgprojetos{
     width: 258px;
     height: 215px;
     padding: 10px;
     border-radius: 20px;
     background: darkgray;
+}
+
+@media (max-width: 480px) {
+    .imgprojetos{
+        width: 100px;
+        height: 85px;
+        padding: 5px;
+        border-radius: 10px;
+    }
 }
 
 .linkprojeotos{
@@ -172,11 +250,26 @@ header{
     background: darkgray;
 }
 
+@media (max-width: 480px) {
+    .figprojetos{
+        font-size: 13px;
+        width: 100%;
+    }
+}
+
 .imgselfcare{
     width: 160px;
     height: 100px;
     padding-top: 30px;
     background: darkgray;
+}
+
+@media (max-width: 480px) {
+    .imgselfcare{
+        width: 100px;
+        height: 70px;
+        padding: 5px;     
+    }
 }
 
 .mainsobre{
@@ -195,7 +288,13 @@ header{
     padding-top: 30px;
     font-size: 30px;
     width: 700px;
+}
 
+@media (max-width: 768px) {
+    .h2sobre{
+        font-size: 24px;
+        width: 80%;
+    }
 }
 
 .habilidade{
@@ -216,4 +315,10 @@ header{
     flex-direction: row;
 }
 
-`
+@media (max-width: 600px) {
+  .divsobre {
+    flex-direction: column;
+  }
+}
+
+`;
